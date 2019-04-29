5fe6210087a78e6075bc97dae89644080c1f5fd4
Mark Skinner <markskinner850@gmail.com>
2019-04-29 14:53:11 -0400

    Comment out redux chrome extention
    The extention code prevents the app from
    working in browsers other than chrome

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const DrumPad = props => (
-  <div id={props.name.replace(" ", "-").toLowerCase()} className={`btn btn-custom btn-lg btn-drum drum-pad ${props.animation}`} onClick={props.handleClick}>
-    <audio id={props.keyPress} className='clip' src={props.audioClip} preload="auto"></audio>
+  <div id={props.name.replace(' ', '-').toLowerCase()} className={`btn btn-custom btn-lg btn-drum drum-pad ${props.animation}`} onClick={props.handleClick}>
+    <audio id={props.keyPress} className='clip' src={props.audioClip} preload='auto'></audio>
     {props.keyPress}
   </div>
 );
