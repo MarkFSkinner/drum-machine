4546d9da04391ddc8847c9c61f0a3a9d49d974f0
Mark Skinner <markskinner850@gmail.com>
2019-04-29 12:49:21 -0400

    Change style & color scheme

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const DrumPad = props => (
-  <div id={props.name.replace(" ", "-").toLowerCase()} className={`btn btn-secondary btn-lg btn-drum drum-pad ${props.animation}`} onClick={props.handleClick}>
+  <div id={props.name.replace(" ", "-").toLowerCase()} className={`btn btn-custom btn-lg btn-drum drum-pad ${props.animation}`} onClick={props.handleClick}>
     <audio id={props.keyPress} className='clip' src={props.audioClip} preload="auto"></audio>
     {props.keyPress}
   </div>
--- src/components/Power.js
+++ src/components/Power.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Power = props => (
   <div id='power'>
     <div id='power-btns' className="btn-group btn-group-toggle" data-toggle="buttons">
-      <label className="btn btn-secondary" onClick={props.powerOff}>
+      <label className="btn btn-toggle" onClick={props.powerOff}>
         <input type="radio" name="options" id="option1" autoComplete="off" defaultChecked /> Off
       </label>
-      <label className="btn btn-secondary active" onClick={props.powerOn}>
+      <label className="btn btn-toggle active" onClick={props.powerOn}>
         <input type="radio" name="options" id="option2" autoComplete="off" /> On
       </label>
     </div>
